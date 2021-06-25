c83b7f07e3e7613ac15450a0060bdac179200500
chaouki bencherif <chaouki.bencherif1@ibm.com>
2021-06-25 14:46:29 +0100

    first commit

---
+++ MyLibrary/MyLibrary.swift
@@ -0,0 +1,10 @@
+//
+//  MyLibrary.swift
+//  MyLibrary
+//
+//  Created by Chaouki Benchrif on 24/6/2021.
+//
+
+class MyLibrary {
+
+}
---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.2
+import PackageDescription
+let package = Package(
+    name: "MyLibrary",
+    products: [
+        .library(name: "MyLibrary", targets: ["MyLibrary"])
+    ],
+    targets: [
+        .target(name: "MyLibrary", path: "MyLibrary")
+    ]
+)
